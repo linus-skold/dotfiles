6b0d3d03a0f186ff91818f51828eb6ae2d06df33
Linus Sköld <contact@linusskold.com>
2026-09-23 09:44:50 +0200

    feat(mise): updated mise config, removed tasks, delted readme, moved iis show task to file

---
+++ mise/tasks/iis/show.ps1
@@ -0,0 +1,16 @@
+
+Import-Module WebAdministration
+
+Get-Website | ForEach-Object {
+    $site = $_
+    $bindings = ($site.Bindings.Collection | ForEach-Object {
+        $_.bindingInformation
+    }) -join "; "
+
+    [PSCustomObject]@{
+        Name        = $site.Name
+        State       = $site.State
+        PhysicalPath= $site.PhysicalPath
+        Bindings    = $bindings
+    }
+}
